a3dc6e19d0f35ea5e97f91b9f80e12315f48af94
luchosr <luchosr@gmail.com>
2021-08-10 09:27:41 +0200

    add content component

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Content from "./Content/Content";
 import Header from "./Header/Header";
 
 const App = () => {
@@ -14,15 +15,11 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
