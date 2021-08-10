6ed4107d337f2be939cdd1d9198908ce56bc18e5
luchosr <luchosr@gmail.com>
2021-08-10 09:43:49 +0200

    add part component

--- part1/courseinfo/src/Content/Content.js
+++ part1/courseinfo/src/Content/Content.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Part from "../Part/Part";
 
-const Content = ({ part, exercises }) => {
+const Content = ({
+  part1,
+  part2,
+  part3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) => {
   return (
-    <p>
-      {part} {exercises}
-    </p>
+    <>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </>
   );
 };
 
