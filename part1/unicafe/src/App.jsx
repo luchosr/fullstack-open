eac17f6b473641c5a753b86952f1577cc1f6bf8f
Luciano Ramello <luchosr@gmail.com>
2024-02-12 17:54:55 +0100

    update statistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,11 +12,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h3>Statistics</h3>
-      <h4>Good {good}</h4>
-      <h4>Neutral {neutral}</h4>
-      <h4>Bad {bad}</h4>
-      <h4>All: {good + neutral + bad}</h4>
+
       <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-
 const Statistics = ({ good, bad, neutral }) => {
   return (
     <>
-      <h4>Average {(good - bad) / (good + neutral + bad) || 0}</h4>
-      <h4>Positive: {good / (good + neutral + bad) || 0} %</h4>
+      <h2>Statistics</h2>
+      {good + bad + neutral === 0 ? (
+        <h3>No feedback given</h3>
+      ) : (
+        <div>
+          <h4>Good {good}</h4>
+          <h4>Neutral {neutral}</h4>
+          <h4>Bad {bad}</h4>
+          <h4>All: {good + neutral + bad}</h4>
+          <h4>Average {(good - bad) / (good + neutral + bad) || 0}</h4>
+          <h4>Positive: {good / (good + neutral + bad) || 0} %</h4>
+        </div>
+      )}
     </>
   );
 };
