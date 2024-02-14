3f6923e99d033ed14c7df9c831443da1327aaf45
Luciano Ramello <luchosr@gmail.com>
2024-02-14 12:04:05 +0100

    update 2.2

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Total = ({ parts }) => {
   return (
-    <p>
-      Number of exercises{' '}
-      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-    </p>
+    <strong>
+      Total of {parts[0].exercises + parts[1].exercises + parts[2].exercises}{' '}
+      exercises.
+    </strong>
   );
 };
 
