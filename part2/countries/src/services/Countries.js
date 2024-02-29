d76cf9216cf7e90a7e5ddf5135086238eb21f299
Luciano Ramello <luchosr@gmail.com>
2024-02-29 14:01:30 +0100

    adds open weather api call

--- part2/countries/src/services/Countries.js
+++ part2/countries/src/services/Countries.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all';
 
+// const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capitalCity}&appid=${
+//   import.meta.env.VITE_OPENWEATHER_KEY
+// }`;
+
 export const getAll = () => {
   return axios.get(baseUrl);
 };
@@ -15,4 +19,12 @@ export const deleteOne = (id) => {
 
 export const update = (id, newObject) => {
   return axios.put(`${baseUrl}/${id}`, newObject);
+};
+
+export const getCapitalWeather = (city) => {
+  return axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+      import.meta.env.VITE_OPENWEATHER_KEY
+    }`
+  );
 };
