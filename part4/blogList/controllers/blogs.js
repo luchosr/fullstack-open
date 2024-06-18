b76e642933a65b4ae66a60c905c69d3d38d4e5ed
Luciano Ramello <luchosr@gmail.com>
2024-06-18 18:14:13 +0200

    WIP 4.19

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 const getTokenFrom = (request) => {
   const authorization = request.get('authorization');
@@ -9,7 +10,6 @@ const getTokenFrom = (request) => {
   }
   return null;
 };
-
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
@@ -22,8 +22,11 @@ blogsRouter.get('/:id', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
-
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   console.log('El user  es: ', user);
   if (!body.title) {
