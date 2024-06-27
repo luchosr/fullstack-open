a2cac8a56b13ad12b5ce504f3bb803a2e84d37ce
Luciano Ramello <luchosr@gmail.com>
2024-06-27 12:55:13 +0200

    Update 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,10 +1,53 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}{' '}
-    <a href={blog.url} target="_blank">
-      link
-    </a>
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [toggleDetails, setToggleDetails] = useState(false);
+
+  return (
+    <>
+      <div
+        style={{
+          border: '1px solid black',
+          width: '50%',
+          borderRadius: '5px',
+          padding: 5,
+          margin: 10,
+        }}
+      >
+        {blog.title} {blog.author}
+        <button
+          style={{ marginLeft: 5 }}
+          type="button"
+          onClick={() => setToggleDetails(!toggleDetails)}
+        >
+          {toggleDetails ? 'hide' : 'view'}
+        </button>
+      </div>
+      {toggleDetails ? (
+        <div
+          style={{
+            border: '1px solid black',
+            width: '50%',
+            borderRadius: '5px',
+            padding: 5,
+            margin: 10,
+          }}
+        >
+          <a
+            style={{ display: 'block', overflow: 'hidden' }}
+            href={blog.url}
+            target="_blank"
+          >
+            {blog.url}
+          </a>
+          <p>Likes: {blog.likes}</p>
+          <p>Author: {blog.author}</p>
+        </div>
+      ) : (
+        ''
+      )}
+    </>
+  );
+};
 
 export default Blog;
