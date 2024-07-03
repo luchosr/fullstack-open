bf8756c5a00a7218a79ad7665c494298ae71160d
Luciano Ramello <luchosr@gmail.com>
2024-07-03 17:39:46 +0200

    update 5.14

--- part5/bloglist-frontend/src/components/Blog.unit.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.unit.test.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable linebreak-style */
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 test('renders content', () => {
@@ -38,4 +40,30 @@ test('blog component should show only a title and an blog author ', () => {
   );
   expect(h4).toHaveTextContent('luciano');
   expect(details).toHaveStyle('display: none');
+});
+
+//siempre cuando hay un click es asíncrono (async / await)
+test('blog should show blog url and likes when show details button is clicked', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'luciano',
+    url: 'www.testing.com',
+    likes: 4,
+  };
+
+  const mockHandler = vi.fn();
+  const { container } = render(<Blog blog={blog} updateView={mockHandler} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText('Show details');
+
+  await user.click(button);
+  const div = container.querySelector('.togglableContent');
+
+  const detailsUrl = container.querySelector('.details-url');
+  const detailsLikes = container.querySelector('.blog-likes');
+
+  expect(div).not.toHaveStyle('display: none');
+  expect(detailsUrl).toHaveTextContent('www.testing.com');
+  expect(detailsLikes).toHaveTextContent('Likes: 4');
 });
