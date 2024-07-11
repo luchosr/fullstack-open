0f84dcc24a02ebe6c1049e3ccd7d949b0b51226f
Luciano Ramello <luchosr@gmail.com>
2024-07-11 11:38:37 +0200

    adds getter function for anecdotes

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -2,9 +2,10 @@ import ReactDOM from 'react-dom/client';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import App from './App';
-import anecdotesReducer from './reducers/anecdotesReducer';
+import anecdotesReducer, { appendAnecdote } from './reducers/anecdotesReducer';
 import filterReducer from './reducers/filterReducer';
 import notificationsReducer from './reducers/notificationsReducer';
+import anecdotesService from './services/anecdotes';
 
 const store = configureStore({
   reducer: {
@@ -13,6 +14,12 @@ const store = configureStore({
     notification: notificationsReducer,
   },
 });
+
+anecdotesService.getAll().then((anecdotes) =>
+  anecdotes.forEach((anecdote) => {
+    store.dispatch(appendAnecdote(anecdote));
+  })
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
