fc9fb986b2cfaaa27f05098f19a8b7dc65af17be
Luciano Ramello <luchosr@gmail.com>
2024-07-11 12:44:39 +0200

    adds ancedotes update

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,22 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Filter from './components/Filter';
 import Notification from './components/Notification';
 
+import anecdotesService from './services/anecdotes';
+import { setAnecdotes } from './reducers/anecdotesReducer';
+
 const App = () => {
   const newNotification = useSelector((state) => state.notification);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdotesService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  }, []);
   return (
     <div>
       <h2>Anecdotes</h2>
