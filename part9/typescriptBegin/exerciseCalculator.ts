aff88251bf295a521d1895bf679382b89981c64e
Luciano Ramello <luchosr@gmail.com>
2024-07-21 13:42:25 +0200

    9.2 WIP

---
+++ part9/typescriptBegin/exerciseCalculator.ts
@@ -0,0 +1,11 @@
+// Escribe una función calculateExercises que calcule el tiempo promedio de las horas diarias de ejercicio
+//  y lo compare con la cantidad objetivo de horas diarias y devuelva un objeto
+
+// { periodLength: 7,
+//     trainingDays: 5,
+//     success: false,
+//     rating: 2,
+//     ratingDescription: 'not too bad but could be better',
+//     target: 2,
+//     average: 1.9285714285714286
+//   }
