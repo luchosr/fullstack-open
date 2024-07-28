cbef411b27443d6c96fcb78085c332fad0f6af1c
Luciano Ramello <luchosr@gmail.com>
2024-07-28 13:43:14 +0200

    updates api methods

--- part9/notes-typescript/src/App.tsx
+++ part9/notes-typescript/src/App.tsx
@@ -1,26 +1,27 @@
-import axios from "axios";
-
+import { getAllNotes, createNote } from "./services/noteService";
 import { useEffect, useState } from "react";
-import { Note } from "./types";
+import { Note, NotesAppText } from "./types";
 
 const App = () => {
   const [newNote, setNewNote] = useState("");
   const [notes, setNotes] = useState<Note[]>([]);
 
   useEffect(() => {
-    axios.get<Note[]>("http://localhost:3001/notes").then((response) => {
-      setNotes(response.data as Note[]);
+    getAllNotes().then((data) => {
+      setNotes(data);
     });
   }, []);
 
   const noteCreation = (event: React.SyntheticEvent) => {
     event.preventDefault();
-    axios
-      .post<Note>("http://localhost:3001/notes", { content: newNote })
-      .then((response) => {
-        setNotes(notes.concat(response.data));
-      });
+    createNote({ content: newNote }).then((data) => {
+      setNotes(notes.concat(data));
+    });
     setNewNote("");
+  };
+
+  const noteRemoval = (noteToRemove: Note) => {
+    console.log("la note es: ", noteToRemove);
   };
 
   return (
@@ -30,11 +31,16 @@ const App = () => {
           value={newNote}
           onChange={(event) => setNewNote(event.target.value)}
         />
-        <button type="submit">add</button>
+        <button type="submit">{NotesAppText.Add}</button>
       </form>
       <ul>
         {notes.map((note) => (
-          <li key={note.id}>{note.content}</li>
+          <li key={note.id}>
+            {note.content}{" "}
+            <button type="button" onClick={() => noteRemoval(note)}>
+              {NotesAppText.Remove}
+            </button>
+          </li>
         ))}
       </ul>
     </div>
