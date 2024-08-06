f637af48a8492886c194d1560795b88fa7e4ceea
Luciano Ramello <luchosr@gmail.com>
2024-08-06 16:08:41 +0200

    updates diagnoses routes

--- part9/patientor/patientor-back/index.ts
+++ part9/patientor/patientor-back/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 import patientsRouter from "./src/routes/patients";
+import diagnosesRouter from "./src/routes/diagnoses";
 const app = express();
 app.use(express.json());
 
@@ -12,6 +13,7 @@ app.get("/ping", (_req, res) => {
 });
 
 app.use("/api/patients", patientsRouter);
+app.use("/api/diagnoses", diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
