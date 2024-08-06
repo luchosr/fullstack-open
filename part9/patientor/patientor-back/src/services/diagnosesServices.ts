e33d1e3516a5fb75864ed3816c5b02c7182d7316
Luciano Ramello <luchosr@gmail.com>
2024-08-06 19:11:18 +0200

    adds diagnoses service

---
+++ part9/patientor/patientor-back/src/services/diagnosesServices.ts
@@ -0,0 +1,22 @@
+import diagnosesData from "../../data/diagnoses";
+
+import { Diagnose } from "../types";
+
+const diagnoses: Diagnose[] = diagnosesData;
+
+const getDiagnoses = (): Diagnose[] => {
+  return diagnoses.map(({ code, name, latin }) => ({
+    code,
+    name,
+    latin,
+  }));
+};
+
+const addDiagnoses = () => {
+  return null;
+};
+
+export default {
+  getDiagnoses,
+  addDiagnoses,
+};
