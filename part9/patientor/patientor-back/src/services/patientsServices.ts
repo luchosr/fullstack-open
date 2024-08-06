537654bb91c936a2c72d8277b069b0a450b09546
Luciano Ramello <luchosr@gmail.com>
2024-08-06 19:10:46 +0200

    updates patients service

---
+++ part9/patientor/patientor-back/src/services/patientsServices.ts
@@ -0,0 +1,14 @@
+import patientsData from "../../data/patients.json";
+
+const getPatients = () => {
+  return patientsData;
+};
+
+const addPatient = () => {
+  return null;
+};
+
+export default {
+  getPatients,
+  addPatient,
+};
