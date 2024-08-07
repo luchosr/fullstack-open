e17d3414d844294f37955ff1337e1e691c889be4
Luciano Ramello <luchosr@gmail.com>
2024-08-07 11:12:08 +0200

    ping fetch removal

--- part9/patientor/patientor-back/index.ts
+++ part9/patientor/patientor-back/index.ts
@@ -9,11 +9,6 @@ app.use(express.json());
 
 const PORT = 3000;
 
-app.get("/ping", (_req, res) => {
-  console.log("someone pinged here");
-  res.send("pong");
-});
-
 app.use("/api/patients", patientsRouter);
 app.use("/api/diagnoses", diagnosesRouter);
 
