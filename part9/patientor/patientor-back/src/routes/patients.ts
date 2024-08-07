4a19a693637b6acb9efeabf71083c710a4eb6683
Luciano Ramello <luchosr@gmail.com>
2024-08-07 12:19:23 +0200

    adds find by id and post routes

--- part9/patientor/patientor-back/src/routes/patients.ts
+++ part9/patientor/patientor-back/src/routes/patients.ts
@@ -1,14 +1,32 @@
 import express from "express";
-import patientSetvices from "../services/patientsServices";
+import patientServices from "../services/patientsServices";
 
 const router = express.Router();
 
-router.get("/", (_req, res) => {
-  res.send(patientSetvices.getPatients());
+router.get("/:id", (req, res) => {
+  const patient = patientServices.findById(String(req.params.id));
+
+  if (patient) {
+    res.send(patient);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
-router.post("/", (_req, res) => {
-  res.send(patientSetvices.addPatient());
+router.get("/", (_req, res) => {
+  res.send(patientServices.getPatients());
+});
+
+router.post("/", (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const addedpatient = patientServices.addPatient(
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation
+  );
+  res.send(addedpatient);
 });
 
 export default router;
