6690ec6849507c9e5d9c7ceff32cd87654b98073
Luciano Ramello <luchosr@gmail.com>
2024-08-07 12:20:22 +0200

    updates patient services

--- part9/patientor/patientor-back/src/services/patientsServices.ts
+++ part9/patientor/patientor-back/src/services/patientsServices.ts
@@ -1,6 +1,7 @@
 import patientsData from "../../data/patients";
+import { v1 as uuid } from "uuid";
 
-import { Patient } from "../types";
+import { Patient, Gender } from "../types";
 
 const patients: Patient[] = patientsData;
 
@@ -8,11 +9,33 @@ const getPatients = (): Patient[] => {
   return patients;
 };
 
-const addPatient = () => {
-  return null;
+const findById = (id: string): Patient | undefined => {
+  const patient = patients.find((d) => d.id === id);
+  return patient;
+};
+
+const addPatient = (
+  name: string,
+  dateOfBirth: string,
+  ssn: string,
+  gender: Gender,
+  occupation: string
+): Patient => {
+  const newPatientEntry = {
+    id: uuid(),
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  };
+
+  patients.push(newPatientEntry);
+  return newPatientEntry;
 };
 
 export default {
   getPatients,
   addPatient,
+  findById,
 };
