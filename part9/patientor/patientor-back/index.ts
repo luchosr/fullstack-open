c98b4d60dc545214cb833b2690d46f0a1fab566c
Luciano Ramello <luchosr@gmail.com>
2024-08-07 10:01:23 +0200

    updates cors policy

--- part9/patientor/patientor-back/index.ts
+++ part9/patientor/patientor-back/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+const cors = require("cors");
 
 import patientsRouter from "./src/routes/patients";
 import diagnosesRouter from "./src/routes/diagnoses";
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
