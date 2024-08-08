43b0acdb608facdac076b10f2cd87e7e69c331d0
Luciano Ramello <luchosr@gmail.com>
2024-08-08 11:17:23 +0200

    updates utils methods

--- part9/patientor/patientor-back/src/utils.ts
+++ part9/patientor/patientor-back/src/utils.ts
@@ -1,29 +1,15 @@
-import { Gender, Patient } from "./types";
-
-export const toNewPatientEntry = (object: Patient | unknown): Patient => {
-  if (!object || typeof object !== "object") {
-    throw new Error("Incorrect or missing data");
-  }
-  const newPatientEntry: Patient = {
-    name: parseName(object.name),
-    dateOfBirth: parseDate(object.date),
-    ssn: "090786-122X",
-    gender: "male",
-    occupation: "New york city cop",
-  };
-  return newPatientEntry;
-};
+import { Gender, NewPatientEntry } from './types';
 
 const parseName = (name: unknown): string => {
   if (!name || !isString(name)) {
-    throw new Error("Incorrect or missing comment");
+    throw new Error('Incorrect or missing comment');
   }
 
   return name;
 };
 
 const isString = (text: unknown): text is string => {
-  return typeof text === "string" || text instanceof String;
+  return typeof text === 'string' || text instanceof String;
 };
 
 const isDate = (date: string): boolean => {
@@ -32,20 +18,60 @@ const isDate = (date: string): boolean => {
 
 const parseDate = (date: unknown): string => {
   if (!date || !isString(date) || !isDate(date)) {
-    throw new Error("Incorrect or missing date: " + date);
+    throw new Error('Incorrect or missing date: ' + date);
   }
   return date;
-};
-
-const parseGender = (gender: unknown): Gender => {
-  if (!gender || !isString(gender) || !isGender(gender)) {
-    throw new Error("Incorrect or missing weather: " + gender);
-  }
-  return gender;
 };
 
 const isGender = (param: string): param is Gender => {
   return Object.values(Gender)
     .map((v) => v.toString())
     .includes(param);
+};
+
+const parseGender = (gender: unknown): Gender => {
+  if (!gender || !isString(gender) || !isGender(gender)) {
+    throw new Error('Incorrect or missing gender: ' + gender);
+  }
+  return gender;
+};
+
+const isEmptyString = (text: string): boolean => {
+  return text.trim().length === 0;
+};
+
+const parseData = (data: unknown, name: string): string => {
+  if (!isString(data)) {
+    throw new Error(`Incorrect data in field: '${name}'`);
+  }
+
+  if (isEmptyString(data)) {
+    throw new Error(`Missing value in field: '${name}'`);
+  }
+
+  return data;
+};
+
+export const toNewPatientEntry = (object: unknown): NewPatientEntry => {
+  if (!object || typeof object !== 'object') {
+    throw new Error('Incorrect or missing data');
+  }
+
+  if (
+    'name' in object &&
+    'dateOfBirth' in object &&
+    'ssn' in object &&
+    'gender' in object &&
+    'occupation' in object
+  ) {
+    const newPatientEntry: NewPatientEntry = {
+      name: parseName(object.name),
+      dateOfBirth: parseDate(object.dateOfBirth),
+      ssn: parseData(object.ssn, 'ssn'),
+      gender: parseGender(object.gender),
+      occupation: parseData(object.occupation, 'occupation'),
+    };
+    return newPatientEntry;
+  }
+  throw new Error('Incorrect data: a field missing');
 };
