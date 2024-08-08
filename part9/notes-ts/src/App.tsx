5f8b72f1885c52887f35d5f74c3fe08dca03f289
Luciano Ramello <luchosr@gmail.com>
2024-08-08 19:43:42 +0200

    update setters and getters

--- part9/notes-ts/src/App.tsx
+++ part9/notes-ts/src/App.tsx
@@ -1,24 +1,23 @@
 import { useEffect, useState } from 'react';
 import { Note } from './types';
-import axios from 'axios';
+import { getAllNotes, createNote } from './services/noteService';
 
 export const App = () => {
   const [newNote, setNewNote] = useState('');
   const [notes, setNotes] = useState<Note[]>([{ id: 1, content: 'testing' }]);
 
   useEffect(() => {
-    axios.get<Note[]>('http://localhost:3000/notes').then((response) => {
-      setNotes(response.data);
+    getAllNotes().then((data) => {
+      setNotes(data);
     });
   }, []);
 
   const noteCreation = (event: React.SyntheticEvent) => {
     event.preventDefault();
-    axios
-      .post<Note>('http://localhost:3001/notes', { content: newNote })
-      .then((response) => {
-        setNotes(notes.concat(response.data));
-      });
+    createNote({ content: newNote }).then((data) => {
+      setNotes(notes.concat(data));
+    });
+
     setNewNote('');
   };
 
